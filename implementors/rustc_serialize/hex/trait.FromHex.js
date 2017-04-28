a32432f9f03a21c61805eb0525c193f506ddeb00
Travis CI User <travis@example.org>
2017-04-28 13:13:27 +0000

    Update documentation

---
+++ implementors/rustc_serialize/hex/trait.FromHex.js
@@ -0,0 +1,20 @@
+(function() {var implementors = {};
+implementors["bitcrypto"] = [];
+implementors["db"] = [];
+implementors["import"] = [];
+implementors["message"] = [];
+implementors["network"] = [];
+implementors["p2p"] = [];
+implementors["rpc"] = [];
+implementors["rustc_serialize"] = [];
+implementors["script"] = [];
+implementors["serialization"] = [];
+implementors["verification"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
